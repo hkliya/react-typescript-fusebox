419f70ebe6c14ccb29873b50643f9e013b2c157c
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-09 10:40:27 +0800

    React works!

--- fuse.js
+++ fuse.js
@@ -1,10 +1,16 @@
-const { FuseBox } = require('fuse-box');
+const {FuseBox, WebIndexPlugin, BabelPlugin} = require('fuse-box');
 
 const fuse = FuseBox.init({
-  homeDir: './src',
-  output: './dist/$name.js',
-    });
+    homeDir: './src',
+    output: './dist/$name.js',
+    plugins: [
+        BabelPlugin(),
+        WebIndexPlugin({
+            template: './src/index.html'
+        })
+    ]
+});
 
 fuse.dev();
-fuse.bundle('app').instructions('>index.js');
+fuse.bundle('app').watch().instructions('> index.jsx');
 fuse.run();
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <p>Let's Rock!</p>
+        );
+    }
+}
+
+export default App;
